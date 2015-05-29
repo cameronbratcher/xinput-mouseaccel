1ecc1893b979f67ee98698c6c2efa364ae54ddda
Cameron Bratcher <cameronbratcher@gmail.com>
2015-05-28 18:19:16 -0700

    Update disablemouseaccel.sh

--- disablemouseaccel.sh
+++ disablemouseaccel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#wait for the desktop to settle
+#wait for the desktop to load
 sleep 5
-# turn off mouse acceleration
+#disables mouse accel
 xinput set-prop changeme 'Evdev Middle Button Emulation' 0
 xinput set-prop changeme 'Device Accel Profile' -1
 xinput set-prop changeme 'Device Accel Velocity Scaling' 1
